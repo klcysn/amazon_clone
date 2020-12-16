0b3f154e6b72191fc5cb4e4a45f3b406442bcb29
klcysn <klcysn@users.noreply.github.com>
2020-12-16 20:42:33 +0100

    Complete Searchbar and start home page

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import "./Home.scss"
+import ProductCard from "../../components/ProductCard/ProductCard"
+
+
+export const Home = () =>{
+    return(
+        <div className="home-container">
+           <ProductCard />  
+        </div>
+    )
+}
